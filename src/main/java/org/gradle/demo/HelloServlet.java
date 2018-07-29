cb15d06f63d14183ce41189a3a56c89c19bd7ffc
Gavin King <gavin.king@itslearning.com>
2018-07-29 16:14:18 +0200

    Added JSON support for API

--- src/main/java/org/gradle/demo/HelloServlet.java
+++ src/main/java/org/gradle/demo/HelloServlet.java
@@ -1,11 +1,16 @@
 package org.gradle.demo;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.Since;
 
 @WebServlet(name = "HelloServlet", urlPatterns = {"hello"}, loadOnStartup = 1)
 public class HelloServlet extends HttpServlet {
@@ -15,7 +20,14 @@ public class HelloServlet extends HttpServlet {
         HttpServletRequest request,
         HttpServletResponse response
     ) throws ServletException, IOException {
-        response.getWriter().print("Hello, World!");
+        Gson gson = new GsonBuilder().setVersion(1.0).setPrettyPrinting().create();
+
+        APIData data = new APIData();
+        data.setMessage("Hello world");
+
+        String json = gson.toJson(data);
+
+        response.getWriter().print(json.toString());
     }
 
     protected void doPost(
@@ -28,5 +40,20 @@ public class HelloServlet extends HttpServlet {
 
         request.setAttribute("user", name);
         request.getRequestDispatcher("response.jsp").forward(request, response);
+    }
+}
+
+
+class APIData {
+    @Since(1.0) private String message;
+
+    public APIData() {}
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
     }
 }
